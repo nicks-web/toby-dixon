655d2822031984648f8329b004726bd5320f41eb
nicks-web <103668870+nicks-web@users.noreply.github.com>
2022-04-14 19:00:40 +0100

    Update Contact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -19,20 +19,7 @@
         />
         <span>{{ panelist }}</span>
       </label>
- <div class="sender-info">
-           <div>
-            <label for="name" class="label">Your name</label>
-            <input type="text" name="name" />
-          </div>
-          <div>
-            <label for="email" class="label">Your email</label>
-            <input type="email" name="email" />
-          </div>
-        </div>
-        <div class="message">
-          <label for="message" class="label">Message</label>
-           <textarea name="message"></textarea>
-         </div>
+
     </form>
   <script>
   import axios from "axios";
