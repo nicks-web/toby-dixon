533e6454d8c63ca0fb962e427359b4609078e87f
nicks-web <103668870+nicks-web@users.noreply.github.com>
2022-04-14 18:55:52 +0100

    Update Contact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -9,16 +9,20 @@
    <form
     ...
     @submit.prevent="handleSubmit">
-    <label v-for="(panelist, index) in panelists" :key="index">
-      <input
-        type="radio"
-        name="panelist"
-        @input="ev => form.askPerson = ev.target.value"
-        :value="panelist"
-        :checked="form.askPerson === panelist"
-      />
-      <span>{{ panelist }}</span>
-    </label>
+    <div class="sender-info">
+           <div>
+            <label for="name" class="label">Your name</label>
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <label for="email" class="label">Your email</label>
+            <input type="email" name="email" />
+          </div>
+        </div>
+        <div class="message">
+          <label for="message" class="label">Message</label>
+           <textarea name="message"></textarea>
+         </div>
     ...
   </form>
 </template>
