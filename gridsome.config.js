e78c0532a270973da5296aa7742ade7f69023c51
nicks-web <103668870+nicks-web@users.noreply.github.com>
2022-04-15 10:56:05 +0100

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,11 +2,11 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteName: "TOBY DIXON",
+  siteUrl: ``,
   host: "0.0.0.0",
   titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
+  siteDescription: "Fine Art Painter",
   plugins: [
     {
       use: "@gridsome/source-filesystem",
